1567f4078cda030756ee870fad1773df0d96a6b8
Victor Jejlskov <victorwilliamjejlskov@gmail.com>
2023-04-16 12:44:22 +0200

    Update favouriteHeartButton.tsx

--- src/components/atoms/favouriteHeartButton.tsx
+++ src/components/atoms/favouriteHeartButton.tsx
@@ -1,4 +1,5 @@
 import { HeartIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 interface FavouriteProps {
   movieId: number;
@@ -6,16 +7,21 @@ interface FavouriteProps {
   onAddToFavourites: (id: string) => void;
 }
 const FavouriteHeartButton = (props: FavouriteProps) => {
+  const { movieId: id, isFavourite, onAddToFavourites } = props;
+  const [heartColor, setHeartColor] = useState(
+    isFavourite ? "#af2b62" : "#ffffff"
+  );
+
   const handleClick = () => {
     onAddToFavourites(id.toString());
+    setHeartColor(isFavourite ? "#ffffff" : "#af2b62");
   };
-  const { movieId: id, isFavourite, onAddToFavourites } = props;
   return (
     <div className="">
       <HeartIcon
         width={25}
         className=""
-        color={isFavourite ? "#af2b62" : "#ffffff"}
+        color={heartColor}
         stroke="#ffffff"
         onClick={handleClick}
       />
