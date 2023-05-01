46f66e526d7b0f3a7141ba862451c676a0a9e1f6
Victor Jejlskov <victorwilliamjejlskov@gmail.com>
2023-05-01 23:51:20 +0200

    Update searchButton.tsx

--- src/components/atoms/searchButton.tsx
+++ src/components/atoms/searchButton.tsx
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 const SearchButton = () => {
   const [searchToggled, setSearchToggled] = useState(true);
+  const ref = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    if (ref.current !== null) {
+      ref.current.focus();
+    }
+  };
 
   return (
     <div className="flex">
       <input
         type="text"
+        ref={ref}
         className={
           searchToggled
             ? "w-0"
             : "input-primary input mr-3 max-h-[80%] w-80 transition-all duration-[600ms] ease-out"
         }
+        onBlur={() => {
+          setSearchToggled(!searchToggled);
+        }}
       />
       <button
         className="btn-ghost btn-circle btn"
         onClick={() => {
+          handleClick();
           setSearchToggled(!searchToggled);
         }}
       >
